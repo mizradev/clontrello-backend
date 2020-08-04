7d766ee3e6322b07fcb3dd5a1d3be261f697727f
luis-rios <vvrhz11@gmail.com>
2020-08-04 11:01:12 -0500

    'correcciones'

--- comments/models.py
+++ comments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from users.models import User
 from cards.models import Cards
 
-# Create your models here.
+
 class Comments(models.Model):
 	"""docstring for Comentarios"""
 	card = models.ForeignKey(Cards, on_delete=models.CASCADE)
@@ -12,4 +12,4 @@ class Comments(models.Model):
 	creation_date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-        return self.name
+		return self.card
--- comments/serializers.py
+++ comments/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 from comments.models import Comments
 from users.serializers import BaseUserSerializer
 
+
 class CommentsSerializer(serializers.ModelSerializer):
 	comments = BaseUserSerializer(read_only=True)
 
-    class Meta:
-	    model = Comments
-	    fields = ['card', 'message', 'owner', 'creation_date']
+
+class Meta:
+	model = Comments
+	fields = ['card', 'message', 'owner', 'creation_date']
