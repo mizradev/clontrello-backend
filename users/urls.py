d172b8eda15d360b870d676f9e63daaf68762a82
soymizra <jorgemizrain@gmail.com>
2020-08-09 23:37:11 -0600

    Se modifico el settings y se agregaron endpoints de Users

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+
+from . import views
+
+urlpatterns = [
+	path('api/users', views.UserListApiView.as_view()),
+	path('api/users/<id>', views.UserSearchApiView.as_view()),
+] 
--- users/views.py
+++ users/views.py
@@ -6,8 +6,27 @@ from rest_framework import viewsets
 from users.models import User
 from users.serializers import UserSerializer
 
+from rest_framework.generics import ListAPIView
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+
+class UserListApiView(ListAPIView):# Lista todos los usuarios
+	serializer_class = UserSerializer
+	def get_queryset(self):
+		return User.objects.all()
+
+class UserSearchApiView(ListAPIView):# regresa un usuario por ID
+	
+	serializer_class = UserSerializer
+
+	def get_queryset(self):
+		id = self.kwargs['id']
+		return User.objects.filter(
+			id__icontains = id
+		)
+
 
